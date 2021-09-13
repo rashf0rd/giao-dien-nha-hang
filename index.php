fc6e74f390e64f9e2bc4a194ef446f4ec6d03f37
rashf0rd <nguyenminhtrong12a0@gmail.com>
2021-09-13 16:52:35 +0700

    tao web, add logo vs menu

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>wow food</title>
+</head>
+<body>
+
+<div id="header">
+    <div class="logo">
+        <img src="image/logo.png" alt="" >
+    <div>
+    <div class="menu">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Foods</a>
+        <a href="#">Contact</a>
+</body>
+</html>
