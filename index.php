6ec0852201b13a94b3f6dfa6f27650eeb54be253
rashf0rd <nguyenminhtrong12a0@gmail.com>
2021-09-16 16:43:59 +0700

    chỉnh sửa header, add style.css, chỉnh sửa style.css

--- index.php
+++ index.php
@@ -2,17 +2,39 @@
 <html>
 <head>
 <title>wow food</title>
+<link rel="stylesheet" href="css/style.css">
+<style>
+    .header{
+        height: 100px;
+    }
+    .logo{
+        float:left;
+        width: 80%;
+        height: 100%;
+    }
+    .menu{
+        float: right;
+        width: 20%;
+        height: 100%;
+    }
+</style>    
 </head>
+
 <body>
 
-<div id="header">
-    <div class="logo">
-        <img src="image/logo.png" alt="" >
-    <div>
-    <div class="menu">
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <a href="#">Foods</a>
-        <a href="#">Contact</a>
+    <div id="header">
+        <div class="logo">
+            <img src="image/logo.png" alt="" width="10%" class="img-responsive" >
+        <div>
+        <div class="menu">
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Foods</a>
+            <a href="#">Contact</a>
+        </div>
+    </div>
+    <div class="clear-float">    
+    </div>
+    <div class="find">
 </body>
 </html>
